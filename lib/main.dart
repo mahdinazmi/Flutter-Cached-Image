80bf21d7924e45d67498f1d12bdac73f11dd4293
AzZaya0 <ajaya.ajyol112@gmail.com>
2023-09-22 12:54:17 +0545

    package updated

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false, 
       title: 'Flutter Demo',
       home: CacheImage()
     );
